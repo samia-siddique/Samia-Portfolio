a3e7b01e19c029ee3d9fde271b4d3e0ad6c3cc64
Samia Siddique <samia.siddique34@gmail.com>
2026-07-21 16:57:32 +0500

    Create Navbar

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Home from './components/Home/Home'
 
 const App = () => {
   return (
-    <div>
-
-    </div>
+    <>
+      <Home/>
+    </>
   )
 }
 
---
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>Projects</li>
+                    <li>Certificates</li>
+                    <li>Notes</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+
+        </div>
+    )
+}
+
+export default Navbar
